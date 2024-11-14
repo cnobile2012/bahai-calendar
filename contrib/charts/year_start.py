75b910c1362533a04d98b99be356a9ef8bc6a384
Carl J. Nobile <carl.nobile@gmail.com>
2024-11-13 20:32:15 -0500

    Created a spreadsheet showing the differences in the Badi Calendar created by Jan R. Greis and my code.

---
+++ contrib/charts/year_start.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# contrib/chart/year_start.py
+#
+# Jan Greis -> jan.r.greis@gmail.com
+#
+
+import os
+import re
+import sys
+import math
+import pprint
+
+PWD = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(PWD))
+sys.path.append(BASE_DIR)
+
+
+class YearStart:
+    """
+    """
+    TXT_FILE = os.path.join(PWD, 'YearStart.txt')
+    CSV_FILE = os.path.join(PWD, 'YearStart.csv')
+    C_RE_TEXT = re.compile(r'(\w+) +(\w+) +(\w+) +(\w+) +(\w+) +(\w+)'
+                           r' +(\w+) +(\w+) +(\w+)')
+    C_RE_DAT0 = re.compile(r'(\(.+\)) +(\(.+\)) +(\d) +([\w`]+) +(\w+) +(\w+)'
+                           r' +(\d{3}) +(\d{3}) +(\d)')
+    C_RE_WHO = re.compile(r'(\w+) +(\w+)')
+    C_RE_DAT1 = re.compile(r'(\w+) +(\w+) +([\w/]+)(?: +(\w+)? +([\w/]+)?)?')
+
+    #def __init__(self):
+    #    self._csv : str
+
+    def start(self):
+        data = []
+        csv = ""
+
+        with open(self.TXT_FILE, mode='r', newline='') as txt_file:
+            for line in txt_file:
+                if (item := self.C_RE_TEXT.search(line)) is not None:
+                    data.append(item.groups())
+                elif (item := self.C_RE_DAT0.search(line)) is not None:
+                    data.append(item.groups())
+                elif (item := self.C_RE_DAT1.search(line)) is not None:
+                    data.append([field if field is not None else ""
+                                 for field in item.groups()])
+                elif (item := self.C_RE_WHO.search(line)) is not None:
+                    item = item.groups()
+                    data.append(["", item[0], "", item[1]])
+
+        for item in data:
+            csv += '|'.join(item) + '\n'
+
+        with open(self.CSV_FILE, 'w') as csv_file:
+            csv_file.write(csv)
+
+        #pprint.pprint(csv)
+
+
+if __name__ == "__main__":
+    ys = YearStart()
+    ys.start()
+
+    sys.exit(0)
