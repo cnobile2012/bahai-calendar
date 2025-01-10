a21b6bbb243743619a9aeed89d11443665bcbdc3
Carl J. Nobile <carl.nobile@gmail.com>
2025-01-09 21:39:23 -0500

    Added more classes in the datetime package.

--- ipython_badi.py
+++ ipython_badi.py
@@ -1,6 +1,6 @@
 # %load ipython_badi.py
 # %load_ext autoreload
-from badidatetime import GregorianCalendar, BahaiCalendar, datetime
+from badidatetime import GregorianCalendar, BahaiCalendar, datetime, date, time, timezone, timedelta, tzinfo
 from badidatetime._timedateutils import _td_utils
 from zoneinfo import ZoneInfo
 import math, time, datetime as dtime
