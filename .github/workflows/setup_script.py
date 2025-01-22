72f59220539d1896054fcbdbbf337017dafd742b
Carl J. Nobile <carl.nobile@gmail.com>
2025-01-22 18:35:03 -0500

    Fixed .github/workflows/setup_script.py.

--- .github/workflows/setup_script.py
+++ .github/workflows/setup_script.py
@@ -4,6 +4,13 @@
 # .github/workflows/setup_script.py 
 #
 
+import os
+import sys
+
+PWD = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(PWD))
+sys.path.append(BASE_DIR)
+
 from badidatetime import enable_geocoder
 
 enable_geocoder(True)
