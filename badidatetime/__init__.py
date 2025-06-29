62b74979aa44d7e02d437764808b6634e6aa820f
Carl J. Nobile <carl.nobile@gmail.com>
2025-06-28 21:53:39 -0400

    Minor update to the docs.

--- badidatetime/__init__.py
+++ badidatetime/__init__.py
@@ -4,6 +4,7 @@
 #
 __docformat__ = "restructuredtext en"
 
+import sys
 import importlib
 import geocoder
 from tzlocal import get_localzone
@@ -54,9 +55,8 @@ def _get_local_coordinates():
 def enable_geocoder(enable=True):
     """
     Enable or disable the geocode query to find the local latitude, longitude,
-    and zone. If this function is never run then the geocoder is nor run the
-    the defaults for `datetime.LOCAL_COORD` and `datetime.LOCAL` as set to be
-    local in Tehran Iran.
+    and zone. If this function is never run then the defaults for
+    `datetime.LOCAL_COORD` and `datetime.LOCAL` will be Tehran Iran locale.
 
     :param bool enable: If True (default) geocoder is run else if False it is
                         not run.
